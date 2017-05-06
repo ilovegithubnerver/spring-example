5fd7b40337b56c383f7afd97ab8de303e75d9d82
letorn <letorn@163.com>
2017-05-07 00:26:07 +0800

    完善事务测试用例

---
+++ spring-example-multi-transaction/src/test/java/com/example/MultiTransactionTest.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import com.example.config.JdbcConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JdbcConfig.class})
+public class MultiTransactionTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private DataSourceTransactionManager transactionManager;
+
+    @Test
+    public void test() {
+        DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
+        try {
+            doAction();
+            doAction2();
+            transactionManager.commit(transaction);
+        } catch (Exception e) {
+            transactionManager.rollback(transaction);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAction() {
+        jdbcTemplate.execute("insert into user(account,password) value('lily','123456')");
+    }
+
+    public void doAction2() {
+        DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
+        try {
+            jdbcTemplate.execute("insert into user(account,password) value('lucy','123456')");
+            fail();
+            transactionManager.commit(transaction);
+        } catch (Exception e) {
+            transactionManager.rollback(transaction);
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void fail() {
+        throw new RuntimeException("系统异常");
+    }
+
+}
---
+++ spring-example-multi-transaction/src/test/java/com/example/SingleTransactionTest.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import com.example.config.JdbcConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JdbcConfig.class})
+public class SingleTransactionTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private DataSourceTransactionManager transactionManager;
+
+    @Test
+    public void test() {
+        // 定义事务类型
+        DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+        // 如果当前没有事务，就新建一个事务，
+        // 如果已经存在一个事务，就使用这个事务中
+        transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        // 获取事务
+        TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
+        try {
+            doAction();// 执行一些数据库操作
+            transactionManager.commit(transaction);// 提交事务，操作成功
+        } catch (Exception e) {
+            transactionManager.rollback(transaction);// 回滚事务，操作失败
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAction() {
+        jdbcTemplate.execute("insert into user(account,password) value('lily','123456')");
+        fail();
+    }
+
+    private void fail() {
+        throw new RuntimeException("系统异常");
+    }
+}
