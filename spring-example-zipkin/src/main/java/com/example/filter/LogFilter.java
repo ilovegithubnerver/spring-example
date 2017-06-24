1064d688987c5c0cb22c034a02d9f2307cca132f
liweitang <liwt@tututech.net>
2017-06-24 10:38:44 +0800

    更新zipkin项目

--- spring-example-zipkin/src/main/java/com/example/filter/LogFilter.java
+++ spring-example-zipkin/src/main/java/com/example/filter/LogFilter.java
@@ -6,10 +6,11 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter
+@WebFilter(urlPatterns = "/*")
 public class LogFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
