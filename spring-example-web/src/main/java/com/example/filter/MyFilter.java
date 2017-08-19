acf7d44149321178764494984494b97d589298f2
liweitang <liwt@tututech.net>
2017-08-19 10:01:48 +0800

    更新web

---
+++ spring-example-web/src/main/java/com/example/filter/MyFilter.java
@@ -0,0 +1,18 @@
+package com.example.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("do something...");
+        filterChain.doFilter(request,response);
+    }
+}
