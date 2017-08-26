b67ba860d3f5844a42a367d2bc08ded4c188c604
liweitang <liwt@tututech.net>
2017-08-26 12:44:43 +0800

    更新job

--- spring-example-job/src/main/java/com/example/quartz/ScheduledJobBuilder.java
+++ spring-example-job/src/main/java/com/example/quartz/ScheduledJobBuilder.java
@@ -3,7 +3,6 @@ package com.example.quartz;
 import com.example.job.MyJob;
 import org.springframework.context.ApplicationContext;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,28 +20,6 @@ public class ScheduledJobBuilder {
         scheduledJobs.add(new ScheduledJob("myJob", "test", MyJob.class, 3000L, 3));
 
         return scheduledJobs;
-    }
-
-    private static List<Class<?>> listAllSuper(Class<?> clazz) {
-        Type t = clazz.getGenericSuperclass();
-        Type[] ts = clazz.getGenericInterfaces();
-        if (t == null && ts.length == 0)
-            return new ArrayList<>(0);
-
-        List<Class<?>> types = new ArrayList<>();
-
-        if (t != null) {
-            Class<?> cls = (Class<?>) t;
-            types.add(cls);
-            types.addAll(listAllSuper(cls));
-        }
-
-        for (Type type : ts) {
-            Class<?> cls = (Class<?>) type;
-            types.add(cls);
-            types.addAll(listAllSuper(cls));
-        }
-        return types;
     }
 
 }
