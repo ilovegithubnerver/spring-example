4d5c1b90f6788144eccb55573f19d3d40a7ab2ff
liweitang <liwt@tututech.net>
2017-09-06 18:41:28 +0800

    更新job

---
+++ spring-example-job/src/main/java/com/example/schedule/ScheduleQueryParams.java
@@ -0,0 +1,14 @@
+package com.example.schedule;
+
+public class ScheduleQueryParams {
+
+    private String jobName;
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+}
