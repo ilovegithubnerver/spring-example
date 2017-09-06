71527cabf08606d23067bc7d03e382ad807f7e42
liweitang <liwt@tututech.net>
2017-09-06 18:41:41 +0800

    更新job

--- spring-example-job/src/main/java/com/example/schedule/ScheduleApi.java
+++ spring-example-job/src/main/java/com/example/schedule/ScheduleApi.java
@@ -2,6 +2,7 @@ package com.example.schedule;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,26 +14,38 @@ public class ScheduleApi {
     ScheduleManager scheduleManager;
 
     @PostMapping("/start")
-    public String start(String jobName) {
-        scheduleManager.start(jobName);
+    public String start(@RequestBody ScheduleQueryParams params) {
+        if (params.getJobName() == null) {
+            return "任务名不能为空";
+        }
+        scheduleManager.start(params.getJobName());
         return "OK";
     }
 
     @PostMapping("/stop")
-    public String stop(String jobName) {
-        scheduleManager.stop(jobName);
+    public String stop(@RequestBody ScheduleQueryParams params) {
+        if (params.getJobName() == null) {
+            return "任务名不能为空";
+        }
+        scheduleManager.stop(params.getJobName());
         return "OK";
     }
 
     @PostMapping("/restart")
-    public String restart(String jobName) {
-        scheduleManager.restart(jobName);
+    public String restart(@RequestBody ScheduleQueryParams params) {
+        if (params.getJobName() == null) {
+            return "任务名不能为空";
+        }
+        scheduleManager.restart(params.getJobName());
         return "OK";
     }
 
     @PostMapping("/run")
-    public String run(String jobName) {
-        scheduleManager.run(jobName);
+    public String run(@RequestBody ScheduleQueryParams params) {
+        if (params.getJobName() == null) {
+            return "任务名不能为空";
+        }
+        scheduleManager.run(params.getJobName());
         return "OK";
     }
 }
