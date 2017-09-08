4785de8c2f322d069c13b2a2c5a7c6c20dcc525c
letorn <letorn@163.com>
2017-09-08 21:04:03 +0800

    更新job

--- spring-example-job/src/main/java/com/example/WebConfig.java
+++ spring-example-job/src/main/java/com/example/WebConfig.java
@@ -7,10 +7,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
 @Import({JooqConfiguration.class, QuartzConfiguration.class, ScheduleConfiguration.class})
 @ComponentScan(basePackages = "com.example")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/");
+    }
 }
